02862db94a32ff6b73c6dbc0c536405eb0fc9044
davonisher <104432882+davonisher@users.noreply.github.com>
2024-02-15 14:52:31 +0100

    Update config.py

--- db/config.py
+++ db/config.py
@@ -4,7 +4,7 @@ def config():
     db = {}
     db['host']='localhost'
     db['user']='postgres'
-    db['database']='10' #enter your pgadmin database name
-    db['password']='katse' #enter your pgadmin database password
+    db['database']='' #enter your pgadmin database name
+    db['password']='' #enter your pgadmin database password
     
     return db
